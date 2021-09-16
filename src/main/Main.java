b20167ef8328315ff95398eb8bc759ba62c20b94
iestyn <il202001@riegate.ac.uk>
2021-09-16 11:59:11 +0100

    This is the shop for my main project

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import GUIs.LoginGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginGUI login = new LoginGUI();
+
+        login.setVisible(true);
+        System.out.println("Hello");
+    }
+
+}
